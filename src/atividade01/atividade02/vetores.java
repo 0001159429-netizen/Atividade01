ff7341d10ef22e1c33c36b9a2b13cfabe3c23d36
Leticia <0001159420@senaimgaluno.com.br>
2025-11-04 11:02:27 -0300

    Adicionando atividades

---
+++ src/atividade01/atividade02/vetores.java
@@ -0,0 +1,28 @@
+package atividade01.atividade02;
+import java.util.Scanner;
+
+public class vetores {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] numeros = new int[5];
+
+        // leitura dos números
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = sc.nextInt();
+        }
+
+        System.out.println("\n=== Números digitados ===");
+
+        // exibição dos números
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Número " + (i + 1) + ": " + numeros[i]);
+        }
+
+        sc.close();
+
+    }
+}
